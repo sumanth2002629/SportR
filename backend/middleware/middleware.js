365c2c18438cc10e60c4051afb94345b4a33eb63
kdesh2001 <kdeshpande2001@gmail.com>
2023-12-14 19:26:11 +0530

    logging update

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -1,13 +1,24 @@
 const logger = require('./logger')
 const fs = require('fs');
+const winston = require('winston');
 
+// Set up Winston logger
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logfile.log' }),
+  ],
+});
 
 const requestLogger = (request, response, next) => {
 
-  fs.appendFileSync('./log.txt', '\nMethod: '+request.method,"utf-8");
-  fs.appendFileSync('./log.txt', '\nPath:  '+ request.path, "utf-8");
-  fs.appendFileSync('./log.txt', '\nBody:  '+ request.body, "utf-8");
-  fs.appendFileSync('./log.txt', "\n---", "utf-8");
+  // fs.appendFileSync('./log.txt', '\nMethod: '+request.method,"utf-8");
+  // fs.appendFileSync('./log.txt', '\nPath:  '+ request.path, "utf-8");
+  // fs.appendFileSync('./log.txt', '\nBody:  '+ request.body, "utf-8");
+  // fs.appendFileSync('./log.txt', "\n---", "utf-8");
+  logger.info('Method: '+request.method)
+  logger.info('Path: '+request.path)
+  logger.info('Body: '+request.body)
   next()
 }
 
